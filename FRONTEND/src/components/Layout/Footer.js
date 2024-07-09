af27f3d8b4855e74b128fca7ee4f8c740f05bcb7
Prathamesh Shetty <shettyprathamesh13@gmail.com>
2024-07-09 11:42:21 +0530

    Footer

--- FRONTEND/src/components/Layout/Footer.js
+++ FRONTEND/src/components/Layout/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
    
 
-<footer className=" bottom-0  left-0 z-20 w-full bg-white border-t border-gray-200 shadow p-6 dark:bg-primary-100 dark:border-slate-50">
+<footer className=" fixed bottom-0  left-0 z-20 w-full bg-white border-t border-gray-200 shadow p-6 dark:bg-primary-100 dark:border-slate-50">
     <p className="text-sm text-center text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()}
     {/* TODO : Change URL once hosted */}
     <a href="https://flowbite.com/" className="hover:text-primary-400 hover:font-bold">GATE™</a>. All Rights Reserved.
